a928ff698e91a96dccee364dd558e492b51d6b4a
unknown <lshi@wshp184msk.corp.mango.ru>
2016-11-17 13:59:05 +0300

    master +firing

--- src/ru/rep1/game/Bullet.java
+++ src/ru/rep1/game/Bullet.java
@@ -6,37 +6,37 @@ import java.awt.*;
  * Created by lshi on 17.11.2016.
  */
 public class Bullet implements Drawable {
-    private int x;
-    private int y;
+    private double x;
+    private double y;
     private double angle;
     private double speed = 2;
 
     @Override
     public void draw(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
-        g2.setColor(Color.ORANGE);
-        g2.fillOval(x, y, 10, 10);
+        g2.setColor(Color.GREEN);
+        g2.fillOval((int)x, (int)y, 5, 5);
         g2.dispose();
     }
 
     public void move() {
-        x += speed * Math.sin(Math.toRadians(angle));
-        y += speed * Math.cos(Math.toRadians(angle));
+        x += speed * Math.cos(Math.toRadians(angle));
+        y -= speed * Math.sin(Math.toRadians(angle));
     }
 
-    public int getX() {
+    public double getX() {
         return x;
     }
 
-    public void setX(int x) {
+    public void setX(double x) {
         this.x = x;
     }
 
-    public int getY() {
+    public double getY() {
         return y;
     }
 
-    public void setY(int y) {
+    public void setY(double y) {
         this.y = y;
     }
 
--- src/ru/rep1/game/Cannon.java
+++ src/ru/rep1/game/Cannon.java
@@ -2,6 +2,7 @@ package ru.rep1.game;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
@@ -9,41 +10,37 @@ import java.awt.geom.Rectangle2D;
  * Created by lshi on 17.11.2016.
  */
 public class Cannon implements Drawable {
-    private int width = 100;
-    private int height = 10;
+    private final Line2D rect;
+    private int width = 50;
     private int x = Constant.GAME_WIDTH / 2;
     private int y = Constant.GAME_HEIGHT - 50;
-    private double angle = -90;
+    private double angle = 90;
     private Color cannonColor = Color.YELLOW;
-    Rectangle2D rect;
 
     public Cannon() {
-        rect = new Rectangle2D.Double();
+        rect = new Line2D.Double();
     }
 
     @Override
     public void draw(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.translate(x, y);
-        g2.rotate(Math.toRadians(angle));
         g2.setColor(cannonColor);
-        rect.setRect(0, 0, width, height);
-        g2.fill(rect);
+        rect.setLine(new Point2D.Double(x, y), new Point2D.Double(x + Math.cos(Math.toRadians(angle)) * width, y - Math.sin(Math.toRadians(angle)) * width));
+        g2.draw(rect);
         g2.dispose();
     }
 
     public void turnLeft() {
-        angle -= 1;
-    }
-
-    public void turnRight() {
         angle += 1;
     }
 
+    public void turnRight() {
+        angle -= 1;
+    }
+
     public Point2D getFirePosition() {
-        Rectangle2D r = new Rectangle2D.Double(x, y, width, height);
-        return new Point2D.Double(r.getCenterX() , r.getCenterY());
+        return rect.getP2();
     }
 
     public double getAngle() {
